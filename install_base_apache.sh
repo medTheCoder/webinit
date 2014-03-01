88974a460882143df806898b2bb90661adf3f3f7
root <root@ip-172-31-7-82.eu-west-1.compute.internal>
2014-03-01 16:27:41 +0000

    first commit

---
+++ install_base_apache.sh
@@ -0,0 +1,14 @@
+echo 'Main web app installation...'
+
+echo ' ---/ update and upgrade'
+apt-get update
+apt-get upgrade
+
+echo ' ---/ install apache2, php, mysql'
+apt-get --yes --force-yes install apache2 php5 mysql-server mysql-client
+apt-get --yes --force-yes install libapache2-mod-php5 php5-mysql
+
+echo ' ---/ install monit'
+apt-get --yes --force-yes install monit
+
+ 
